b50e394ba759f7d5d0db3a5ecc3be514a04837a9
ddings73 <76030391+ddings73@users.noreply.github.com>
2021-01-05 22:11:02 +0900

    Add files via upload

---
+++ Java_live-study/ListNodeToStack/src/main/java/ListNode.java
@@ -0,0 +1,16 @@
+public class ListNode{
+    ListNode next;
+    Integer value, size;
+
+    public ListNode(){
+        next = null;
+        value = null;
+        size = 0;
+    }
+
+    public ListNode(int data){
+        next = null;
+        value = data;
+        size = 0;
+    }
+}
